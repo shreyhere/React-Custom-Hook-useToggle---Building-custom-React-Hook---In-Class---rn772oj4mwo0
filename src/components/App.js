f1b5955c7202242afe6668330624e88da3de1fc5
shreyhere <116891326+shreyhere@users.noreply.github.com>
2023-01-21 12:59:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React,{useCallback,useState} from 'react'
+import React, { useCallback, useState } from 'react'
 import '../styles/App.css';
 import useToggle from './useToggle.js'
 const App = () => {
 
-  
+
 
     const [isTextChanged, setIsTextChanged] = useToggle();
 
-  return(
+    return (
         <button id='button' onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
     );
 }
